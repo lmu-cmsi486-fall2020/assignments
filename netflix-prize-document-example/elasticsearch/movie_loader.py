11a1ed41afc36cf079af623b16d80992cd972f31
dondi <dondi@lmu.edu>
2020-10-21 13:50:30 -0700

    Initial versions of movie loaders.

---
+++ netflix-prize-document-example/elasticsearch/movie_loader.py
@@ -0,0 +1,29 @@
+import csv
+import json
+
+"""
+This program generates direct JSON literals from the source Netflix Prize movie file.
+This allows us to pass the data directly into the ElasticSearch `_bulk` endpoint.
+"""
+
+# For simplicity, we assume that the program runs where the files are located.
+MOVIE_SOURCE = 'movie_titles.csv'
+with open(MOVIE_SOURCE, 'r+', encoding='iso-8859-1') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        id = row[0]
+        year = None if row[1] == 'NULL' else int(row[1])
+        title = ', '.join(row[2:])
+
+        # Note the hardcoded empty `ratings` array—we’ll fill those in later.
+        print(json.dumps({
+            'index': {
+                '_id': id
+            }
+        }))
+
+        print(json.dumps({
+            'id': id,
+            'year': year,
+            'title': title,
+        }, ensure_ascii=False))  # ensure_ascii=False forces UTF-8 encoding.
